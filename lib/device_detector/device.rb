319ec659f41525c87417cf14028219ffab0c2867
Stephan Teeuwen <stephan@adnovation.com>
2017-11-29 17:50:55 +0100

    save start of cleanup

--- lib/device_detector/device.rb
+++ lib/device_detector/device.rb
@@ -5,10 +5,6 @@ class DeviceDetector
       regex_meta.any?
     end
 
-    def name
-      ModelExtractor.new(user_agent, regex_meta).call
-    end
-
     def type
       regex_meta[:device]
     end
@@ -25,6 +21,13 @@ class DeviceDetector
 
     def matching_regex
       regex = regexes.find { |r| user_agent =~ r[:regex] }
+      if regex && regex[:models]
+        model_regex = regex[:models].find { |m| user_agent =~ m[:regex]}
+        if model_regex
+          regex[:device] = model_regex[:device] if model_regex.key?(:device)
+          regex.delete(:models)
+        end
+      end
       regex
     end
 
@@ -32,6 +35,12 @@ class DeviceDetector
       raw_regexes.map do |brand, meta|
         fail "invalid device spec: #{meta.inspect}" unless meta[:regex].is_a? String
         meta[:regex] = build_regex(meta[:regex])
+        if meta.key?(:models)
+          meta[:models].each do |model|
+            fail "invalid model spec: #{model.inspect}" unless model[:regex].is_a? String
+            model[:regex] = build_regex(model[:regex])
+          end
+        end
         meta[:path] = path
         meta
       end
